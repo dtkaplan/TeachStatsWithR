f361180e354fe96d8d92d2182068b83354da2df8
dtkaplan <dtkaplan@gmail.com>
2014-12-16 16:18:00 -0600

    Finished Start Teaching pagination.  I hope!

---
+++ ModelingV2/discreteFun.R
@@ -0,0 +1,30 @@
+# Example
+
+# makeDiscreteFun( "golf"=7, "fus"=8)
+# Let any expression be on the RHS
+# pull out all variable names and make the resulting function 
+# take those as arguments
+
+eval(parse(text=as.character(substitute(b))[3]),list(x=2,y=10))
+[1] TRUE
+Browse[1]> class(a)
+[1] "numeric"
+Browse[1]> a
+[1] 7
+Browse[1]> class(b)
+Error: object 'x' not found
+Browse[1]> class(substitute(b))
+[1] "<-"
+Browse[1]> class(substitute(a ~ 3))
+[1] "call"
+Browse[1]> class(substitute(a))
+[1] "<-"
+
+# return a function that's already vectorized.
+makeDiscreteFun <- function(a,b,c,d){
+  browser()
+  Expressions <- list(...)
+  
+  browser()
+  3+4
+}
